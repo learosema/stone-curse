edf245fb699ae52db5e144312f4cb0e24f05899d
Lea Rosema <lea@lea.codes>
2022-05-21 21:40:01 +0200

    chore: implement getField method

--- src/level.ts
+++ src/level.ts
@@ -15,4 +15,9 @@ export class Level {
       .split('\n')
       .map((row) => row.split('').map((col) => symbols.indexOf(col)));
   }
+  
+  getField(x: number, y: number) {
+    const yMax = level.length - 1;
+    return y < 0 || y > yMax ? 1 : (level[y][x] || 1);
+  }
 }
